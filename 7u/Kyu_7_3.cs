92629aa03616a1d81927a1e237a8ae17c078b323
AlbertGRV <spsza400rub@gmail.com>
2024-11-16 14:19:48 +0300

    Feat: Solve 7u task

---
+++ 7u/Kyu_7_3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Kata
+{
+    public static int SquareDigits(int n)
+    {
+
+        string result = "";
+
+        foreach (char digit in n.ToString())
+        {
+            // Возводим цифру в квадрат и добавляем её в строку
+            result += (int)Math.Pow(char.GetNumericValue(digit), 2);
+        }
+
+        return int.Parse(result);
+    }
+}
